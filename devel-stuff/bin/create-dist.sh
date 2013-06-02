ea245d39a6d53e1f7bf290519a4875f920f7a61a
Henrik Sandklef <hesa@sandklef.com>
2013-06-02 23:33:52 +0200

    check installation and check dist file

--- devel-stuff/bin/create-dist.sh
+++ devel-stuff/bin/create-dist.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BASE_TM_VERSION=""
 
@@ -71,17 +71,74 @@ exec_cmd()
     exit_on_error $? "$COMMAND"
 }
 
-exec_cmd git tag $GIT_TAG_ARG $BASE_TM_VERSION
-exec_cmd ./configure --prefix /opt
-exec_cmd make 
-exec_cmd make dist
+check_install()
+{
+    INSTALL_DIR=$1
 
+    INSTALLED_VERSION=`$INSTALL_DIR/bin/tm-vmm --version`
+    if [ "$BASE_TM_VERSION" != "$INSTALLED_VERSION" ]
+    then
+	echo "Installed version ($INSTALLED_VERSION) differs from the one we've just built ($BASE_TM_VERSION)"
+	return 1
+    fi
+
+    $INSTALL_DIR/bin/tm-vmm --list-clients > /dev/null 2> /dev/null
+    RET=$?
+    if [ $RET -ne 0 ] ; then echo " * list clients failed"; return $RET ; fi
+
+    PDFS=$(ls -1 $INSTALL_DIR/share/testingmachine/doc/*.pdf | wc -l )
+    HTMLS=$(ls -1 $INSTALL_DIR/share/testingmachine/doc/*.html | wc -l)
+
+    NR_MANUALS=3
+    if [ $PDFS -ne $NR_MANUALS ]
+    then
+	echo "Can't find the $NR_MANUALS pdf manuals"
+	return 1
+    fi
+
+    if [ $HTMLS -ne $NR_MANUALS ]
+    then
+	echo "Can't find the $NR_MANUALS html manuals"
+	return 1
+    fi
+}
+
+check_dist()
+{
+    cd tmp
+
+    DIST_FILE=$(ls -1t *.tar.gz | head -1)
+    DIST_DIR=$(ls -1t *.tar.gz | head -1 | sed 's,\.tar\.gz,,g')
+
+    cd $DIST_DIR
+
+    exec_cmd ./configure --prefix /tmp/test-2
+    exec_cmd make all install 
+    exec_cmd check_install /tmp/test-2
+
+    cd -
+
+    cd -
+}
+
+
+exec_cmd git tag $GIT_TAG_ARG $BASE_TM_VERSION
+exec_cmd ./configure --prefix /tmp/test-1
+exec_cmd make all install 
+exec_cmd check_install /tmp/test-1
+exec_cmd make dist
+exec_cmd check_dist
 
 if [ "$PUSH" = true ]
 then
     $DEBUG git push --tags
 fi
 
+
+echo ""
+echo ""
+echo "Dist: ok"
+echo "Ready for relase: tmp/$DIST_FILE"
 
 
 
